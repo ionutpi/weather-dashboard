30ecedbc0318435cd8446be91f47ea0f4954b31e
ionutpi <mltrionut@yahoo.co.uk>
2021-08-15 10:05:20 +0200

    Basic prefect flow

--- read-api/read_api.py
+++ read-api/read_api.py
@@ -1,22 +1,37 @@
 import requests
 import pandas as pd
+from prefect import task, Task, Flow
 
 f = open("api-key.txt","r")
 lines = f.readlines()
 api_key = lines[0]
 
-station = '06184' # 6187 
-start_datetime='2021-08-14T00:00:00Z/..'  # 'datetime': start_datetime
+station = '06184'
 period = 'latest-hour'
+url = ' https://dmigw.govcloud.dk/v2/metObs/collections/observation/items'
 
-url = ' https://dmigw.govcloud.dk/v2/metObs/collections/observation/items' # url for the current api version
-r = requests.get(url, params={'api-key': api_key, 'stationId': station, 'period': period}) # Issues a HTTP GET request
-json = r.json()
-features = json["features"]
 
-data = [item["properties"] for item in features]
+@task
+def read_api(url: str, station: str, period: str) -> dict:
+    r = requests.get(url, params={'api-key': api_key, 'stationId': station, 'period': period})
+    raw_data = r.json()
+    return raw_data
 
-df = pd.DataFrame(data)
-print(df)
-print(df.groupby('parameterId').count())
-print(df.groupby('parameterId').mean())
+@task
+def process_raw_data(raw_data: dict) -> pd.DataFrame:
+    features = raw_data["features"]
+    data = [item["properties"] for item in features]
+    df = pd.DataFrame(data)
+    return df
+
+@task 
+def process_df(df):
+    df.groupby('parameterId').mean().to_csv('data.csv')
+
+with Flow("weather data") as flow:
+    data = read_api(url, station, period)
+    df = process_raw_data(data)
+    process_df(df)
+
+state = flow.run()
+print(state)
